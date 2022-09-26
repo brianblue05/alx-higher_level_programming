41688f67839068974d0fb5c936dd32e8d5ff3fce
Brian Mawira <brianmawira05@gmail.com>
2022-09-26 22:26:08 +0300

    Fix 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list=[]):
+    for x in my_list:
+        if x < 0:
+            return None
+        elif x > idx:
+            return None
+        else:
+            print("{:d}".format(x))
